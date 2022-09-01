bbd27d571802c284268627539621218d0bf6e2b3
Joao <joao.airesmatos@gmail.com>
2022-09-01 17:12:52 +0100

    .

--- js/index.js
+++ js/index.js
@@ -5,31 +5,19 @@ const BASE_URL = 'https://us-central1-vinci-dev-6e577.cloudfunctions.net/api/pub
  * Example JavaScript code that interacts with the page and Web3 wallets
  */
 
- // Unpkg imports
+// Unpkg imports
 const Web3Modal = window.Web3Modal.default;
 const WalletConnectProvider = window.WalletConnectProvider.default;
 const evmChains = window.evmChains;
 const Fortmatic = window.Fortmatic;
-
-// Web3modal instance
 let web3Modal
-
-// Chosen wallet provider given by the dialog window
 let provider;
-
-
-// Address of the selected account
-let selectedAccount;
 
 
 /**
  * Setup the orchestra
  */
 function init() {
-
-  console.log("Initializing example");
-  console.log("WalletConnectProvider is", WalletConnectProvider);
-  console.log("Fortmatic is", Fortmatic);
 
   // Tell Web3modal what providers we have available.
   // Built-in web browser provider (only one can exist as a time)
@@ -51,89 +39,6 @@ function init() {
 
 }
 
-
-/**
- * Kick in the UI action after Web3modal dialog has chosen a provider
- */
-async function fetchAccountData() {
-
-  // Get a Web3 instance for the wallet
-  const web3 = new Web3(provider);
-
-  console.log("Web3 instance is", web3);
-
-  // Get connected chain id from Ethereum node
-  const chainId = await web3.eth.getChainId();
-  // Load chain information over an HTTP API
-  const chainData = await evmChains.getChain(chainId);
-  document.querySelector("#network-name").textContent = chainData.name;
-
-  // Get list of accounts of the connected wallet
-  const accounts = await web3.eth.getAccounts();
-
-  // MetaMask does not give you all accounts, only the selected account
-  console.log("Got accounts", accounts);
-  selectedAccount = accounts[0];
-
-  document.querySelector("#selected-account").textContent = selectedAccount;
-
-  // Get a handl
-  const template = document.querySelector("#template-balance");
-  const accountContainer = document.querySelector("#accounts");
-
-  // Purge UI elements any previously loaded accounts
-  accountContainer.innerHTML = '';
-
-  // Go through all accounts and get their ETH balance
-  const rowResolvers = accounts.map(async (address) => {
-    const balance = await web3.eth.getBalance(address);
-    // ethBalance is a BigNumber instance
-    // https://github.com/indutny/bn.js/
-    const ethBalance = web3.utils.fromWei(balance, "ether");
-    const humanFriendlyBalance = parseFloat(ethBalance).toFixed(4);
-    // // Fill in the templated row and put in the document
-    // const clone = template.content.cloneNode(true);
-    // clone.querySelector(".address").textContent = address;
-    // clone.querySelector(".balance").textContent = humanFriendlyBalance;
-    // accountContainer.appendChild(clone);
-  });
-
-  // Because rendering account does its own RPC commucation
-  // with Ethereum node, we do not want to display any results
-  // until data for all accounts is loaded
-  await Promise.all(rowResolvers);
-
-  // Display fully loaded UI for wallet data
-  document.querySelector("#prepare").style.display = "none";
-  document.querySelector("#connected").style.display = "block";
-}
-
-
-
-/**
- * Fetch account data for UI when
- * - User switches accounts in wallet
- * - User switches networks in wallet
- * - User connects wallet initially
- */
-async function refreshAccountData() {
-
-  // If any current data is displayed when
-  // the user is switching acounts in the wallet
-  // immediate hide this data
-  document.querySelector("#connected").style.display = "none";
-  document.querySelector("#prepare").style.display = "block";
-
-  // Disable button while UI is loading.
-  // fetchAccountData() will take a while as it communicates
-  // with Ethereum node via JSON-RPC and loads chain data
-  // over an API call.
-  document.querySelector("#btn-connect").setAttribute("disabled", "disabled")
-  await fetchAccountData(provider);
-  document.querySelector("#btn-connect").removeAttribute("disabled")
-}
-
-
 /**
  * Connect wallet button pressed.
  */
@@ -142,7 +47,7 @@ async function onConnect() {
   console.log("Opening a dialog", web3Modal);
   try {
     provider = await web3Modal.connect();
-  } catch(e) {
+  } catch (e) {
     console.log("Could not get a wallet connection", e);
     return;
   }
@@ -165,32 +70,6 @@ async function onConnect() {
   await refreshAccountData();
 }
 
-/**
- * Disconnect wallet button pressed.
- */
-async function onDisconnect() {
-
-  console.log("Killing the wallet connection", provider);
-
-  // TODO: Which providers have close method?
-  if(provider.close) {
-    await provider.close();
-
-    // If the cached provider is not cleared,
-    // WalletConnect will default to the existing session
-    // and does not allow to re-scan the QR code with a new wallet.
-    // Depending on your use case you may want or want not his behavir.
-    await web3Modal.clearCachedProvider();
-    provider = null;
-  }
-
-  selectedAccount = null;
-
-  // Set the UI back to the initial state
-  document.querySelector("#prepare").style.display = "block";
-  document.querySelector("#connected").style.display = "none";
-}
-
 
 /**
  * Main entry point.
@@ -198,23 +77,22 @@ async function onDisconnect() {
 window.addEventListener('load', async () => {
   init();
   document.querySelector("#btn-connect").addEventListener("click", onConnect);
-  document.querySelector("#btn-disconnect").addEventListener("click", onDisconnect);
 });
 
 const fetchUsers = () => {
-    console.log(window.location.href.split('/')[window.location.href.split('/').length - 1]);
-    console.log(window.location.href);
-    axios.get(BASE_URL, {
-        params: {
-            url: window.location.href,
-            API_KEY: 'VINCI_DEV_6E577'
-        }, headers: { "Access-Control-Allow-Origin": "*" }
+  console.log(window.location.href.split('/')[window.location.href.split('/').length - 1]);
+  console.log(window.location.href);
+  axios.get(BASE_URL, {
+    params: {
+      url: window.location.href,
+      API_KEY: 'VINCI_DEV_6E577'
+    }, headers: { "Access-Control-Allow-Origin": "*" }
+  })
+    .then(response => {
+      const users = response.data.data;
+      console.log(`GET list users`, users);
     })
-        .then(response => {
-            const users = response.data.data;
-            console.log(`GET list users`, users);
-        })
-        .catch(error => console.error(error));
+    .catch(error => console.error(error));
 };
 
 fetchUsers();
